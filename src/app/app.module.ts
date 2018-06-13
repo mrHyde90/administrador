a893dd400d6f312e6b276ad40899a1c8a139c7f3
jose alfredo <a00516283@gmail.com>
2018-06-13 01:04:29 -0500

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+  	MatCardModule,
+  	MatButtonModule,
+  	MatToolbarModule,
+  	MatExpansionModule,
+  	MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
