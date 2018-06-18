5a255019f74735d195aca519078204b1e9e831bc
jose alfredo <a00516283@gmail.com>
2018-06-18 14:38:19 -0500

    finish the basic of the server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser"); 
+
+app.use((req, res, next) => {
+	res.send("lola22");
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+module.exports = app;
