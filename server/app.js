e78ba3f9ea3436c2e86491c9a1c31517938947e1
jose alfredo <a00516283@gmail.com>
2018-06-18 15:07:12 -0500

    finish the first part

--- server/app.js
+++ server/app.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser"); 
 
-app.use((req, res, next) => {
-	res.send("lola22");
-});
+const instrumentRouter = require("./routes/instruments");
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,5 +20,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use("/api/instruments", instrumentRouter);
 
 module.exports = app;
